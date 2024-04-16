83a98dd4ba81a146283909e307fca45a9283e6c6
danidemis <dani.demichi@gmail.com>
2024-04-16 18:31:31 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,7 @@
 echo "$OPENVPN_USER" | sudo tee /etc/openvpn/credentials
 echo "$OPENVPN_PASSWORD" | sudo tee -a /etc/openvpn/credentials
 
+echo "my name is $USER"
 sudo cp /etc/openvpn/credentials /config/credentials
 #sudo openvpn --config client.ovpn --auth-user-pass /etc/openpvn/credentials &
 
